f7c3924e4e1e8f618f15813eaa81f9f63d6bdfe0
Ariefsetiawan900 <ariefsetiawan8788@gmail.com>
2020-12-31 23:01:02 +0700

    create Chat.js

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,74 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import { MoreVert, SearchOutlined, AttachFile } from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
+
+import "./Chat.css";
 
 function Chat() {
-    return (
-        <div>
-            Ini Chat
+  const [seed, setSeed] = useState("");
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  const sendMessage = (e) => {
+      e.preventDefault();
+      console.log("You type ==>", input);
+  };
+
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at.....</p>
         </div>
-    )
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p className={`chat__message ${true && "chat__reciever"}`}>
+          <span className="chat__name">Aliando</span>
+          Hy Semua
+          <span className="chat__timestamp">3:52pm</span>
+        </p>
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input
+            type="text"
+            placeholder="Type a Message"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button type="submit" onClick={sendMessage}>
+            Send a message
+          </button>
+        </form>
+        <MicIcon />
+      </div>
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
